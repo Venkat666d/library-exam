3a4c1fb276ed527af1dc67e48369d1f88755ecfe
Venkata Rami Reddy <144513582+Venkat666d@users.noreply.github.com>
2025-10-22 15:57:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,25 @@ const app = express();
 app.use(express.json());
 
 let books = [
-  { id: 1, title: "The Alchemist", author: "Paulo Coelho" },
-  { id: 2, title: "Harry Potter", author: "J.K. Rowling" },
-  { id: 3, title: "Atomic Habits", author: "James Clear" }
+  { id: 1, title: "Spiderman", author: "Yasir sir" },
+  { id: 2, title: "Harry Potter", author: "Mubarak sir" },
+  { id: 3, title: "cse", author: "AAdil sir" }
 ];
 
 let users = [
-  { id: 1, name: "Alice", history: ["Harry Potter"] },
-  { id: 2, name: "Bob", history: [] }
+  { id: 1, name: "salman", history: ["Harry Potter"] },
+  { id: 2, name: "sharukh", history: [] }
 ];
 
 app.get("/", (req, res) => {
-  res.send("Library API is working");
+
+ res.send("Library API is working - use /users to get users history and /books for books route ");
 });
 
 app.get("/books", (req, res) => {
   res.json(books);
 });
+
 
 app.get("/users", (req, res) => {
   res.json(users);
